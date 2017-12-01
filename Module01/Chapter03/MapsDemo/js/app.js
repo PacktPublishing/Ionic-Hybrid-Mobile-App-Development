5dcc47987c260aee8ccde6c5fca29a5fd0538c85
hk021 <harshalk@packtpub.com>
2017-12-01 18:32:10 +0530

    Code files

--- Module01/Chapter03/MapsDemo/js/app.js
+++
@@ -1,9 +0,0 @@
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
